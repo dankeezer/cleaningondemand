90e267bfbe636a4185bbab7aba2401ebf98e4e16
Dan Keezer <dankeezer@gmail.com>
2015-10-09 12:16:32 -0400

    contact form with sendgrid

--- application.rb
+++ application.rb
@@ -20,3 +20,39 @@ end
 get "/" do
   erb :root
 end
+
+
+post '/' do 
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => "#{name}<#{sender_email}>",
+      :to => 'dankeezer@gmail.com',
+      :subject =>"#{name} has contacted you from cleaningondemand.com",
+      :body => "#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.sendgrid.net', 
+      :port                 => '587',  
+      :user_name            => ENV['SENDGRID_USERNAME'], 
+      :password             => ENV['SENDGRID_PASSWORD'], 
+      :authentication       => :plain, 
+      :enable_starttls_auto => true,
+      :domain               => 'heroku.com'
+    }    
+  }
+end
